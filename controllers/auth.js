0adaa95bcc84b8215b08fa0d27879a00e4811b6d
kf380 <kevinfavre@gmail.com>
2022-03-18 20:13:30 -0300

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcrypt');
+const { json } = require('express/lib/response');
 const { generateJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/Usuario')
 
 
@@ -33,6 +35,8 @@ const login = async(req,res)=>{
         }
         //Generar el JWT
         const token = await generateJWT(usuario.id)
+
+
         res.json({
             usuario,
             token
@@ -47,7 +51,55 @@ const login = async(req,res)=>{
     
 }
 
+const googleSignIn = async(req, res)=>{
+
+    const {id_token} = req.body;
+try{
+
+    const {correo,nombre,img} = await googleVerify(id_token)
+    
+    let usuario = await Usuario.findOne({correo});
+    if(!usuario){
+        //Tengo que crearlo
+        const data ={
+            nombre,
+            correo, 
+            password: ':P',
+            img,
+            google: true
+        }
+        usuario = new Usuario(data);
+        await usuario.save();
+    }
+
+    // Si el usuario en DB
+    if(!usuario.estado){
+        return res.status(401).json({
+            msg: ' Hable con el admnistrador, usuario bloqueado'
+        })
+    }
+
+     //Generar el JWT
+    const token = await generateJWT(usuario.id)
+
+    res.json({
+        usuario,
+        token
+    })
+
+   
+}catch(error){
+    res.status(400).json({
+        ok:false,
+        msg:'El token no se pudo verificar'
+    })
+}
+
+
+}
+
 
 module.exports ={
-    login
+    login,
+    googleSignIn
 }
